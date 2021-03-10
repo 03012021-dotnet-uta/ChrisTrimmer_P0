f339bbf6ed73031a9cd0daa5ca339ba586022655
Christopher Trimmer <chrisschoolacct@yahoo.com>
2021-03-10 17:31:50 -0500

    Added Project0/SweetSalty

---
+++ Project0/OriginalFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using SweetSalty.Domains;
+
+namespace SweetSalty.Client
+{
+    class Program
+    {
+        // Entry point of Program
+        static void Main(string[] args)
+        {
+            // Create a SweetSalty object from Model
+            SweetSaltyD mySweetSaltyObject = new SweetSaltyD();
+
+            // Perform calculation and print the totals
+            mySweetSaltyObject.Calculate();
+            mySweetSaltyObject.PrintTotal();
+
+            // Ensure shell does not exit before customer can interact
+            Console.ReadLine();
+
+        }
+    }
+}
